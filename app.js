036e2869efa009e5dffc46904e9a0848334f7e98
kevinnail <fumalicious@gmail.com>
2022-09-24 20:54:32 -0700

    nav bar almost done- css bugs to work out but links in place

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const home = document.getElementById('home-link');
 const aboutMe = document.getElementById('about-me-link');
 const gallery = document.getElementById('gallery-link');
-const recipe = document.getElementById('recipe-link');
+const recipes = document.getElementById('recipe-link');
 const contact = document.getElementById('contact-link');
 /* State */
 
@@ -13,28 +13,33 @@ const contact = document.getElementById('contact-link');
 /* Display Functions */
 function updateNavBar() {
     const page = document.URL;
-    switch (page) {
-        case 'index':
-            aboutMe.classList.add('nav-bar');
-            break;
-        case 'about-me':
-            aboutMe.classList.add('nav-bar');
-            break;
-        case 'gallery':
-            aboutMe.classList.add('nav-bar');
-            break;
-        case 'recipe':
-            aboutMe.classList.add('nav-bar');
-            break;
-        case 'about=me':
-            aboutMe.classList.add('nav-bar');
-            break;
-        case 'contact':
-            aboutMe.classList.add('nav-bar');
-            break;
-    }
-    if (document.URL.includes('about-me.html')) {
+
+    // switch (page) {
+    //     case document.URL.includes('/index.html'):
+    //         home.classList.add('nav-bar');
+    //         break;
+    //     case document.url.includes('about-me'):
+    //         aboutMe.classList.add('nav-bar');
+    //         break;
+    //     case 'gallery.html':
+    //         gallery.classList.add('nav-bar');
+    //         break;
+    //     case 'recipe':
+    //         recipes.classList.add('nav-bar');
+    //         break;
+    //     case 'contact':
+    //         contact.classList.add('nav-bar');
+    // }
+    if (document.URL.includes('index.html')) {
+        home.classList.add('nav-bar');
+    } else if (document.URL.includes('about-me.html')) {
         aboutMe.classList.add('nav-bar');
+    } else if (document.URL.includes('gallery.html')) {
+        gallery.classList.add('nav-bar');
+    } else if (document.URL.includes('recipes.html')) {
+        recipes.classList.add('nav-bar');
+    } else if (document.URL.includes('contact.html')) {
+        contact.classList.add('nav-bar');
     }
 }
 updateNavBar();
