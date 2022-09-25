4bfe0fcf8b66f0d645f422a3997fb1c8caee99e6
kevinnail <fumalicious@gmail.com>
2022-09-24 20:19:27 -0700

    creating blank copies of pages with proper nav id's for js update

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const home = document.getElementById('home-link');
+const aboutMe = document.getElementById('about-me-link');
+const gallery = document.getElementById('gallery-link');
+const recipe = document.getElementById('recipe-link');
+const contact = document.getElementById('contact-link');
 /* State */
 
 /* Events */
 
 /* Display Functions */
-
-// (don't forget to call any display functions you want to run on page load!)
+function updateNavBar() {
+    const page = document.URL;
+    switch (page) {
+        case 'index':
+            aboutMe.classList.add('nav-bar');
+            break;
+        case 'about-me':
+            aboutMe.classList.add('nav-bar');
+            break;
+        case 'gallery':
+            aboutMe.classList.add('nav-bar');
+            break;
+        case 'recipe':
+            aboutMe.classList.add('nav-bar');
+            break;
+        case 'about=me':
+            aboutMe.classList.add('nav-bar');
+            break;
+        case 'contact':
+            aboutMe.classList.add('nav-bar');
+            break;
+    }
+    if (document.URL.includes('about-me.html')) {
+        aboutMe.classList.add('nav-bar');
+    }
+}
+updateNavBar();
